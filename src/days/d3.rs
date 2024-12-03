1b5286230b9f3899359b2fc35d3e8b1054287f40
Joel Thibault <thibault@broad.dev>
2024-12-03 18:22:58 -0500

    flat_map

--- src/days/d3.rs
+++ src/days/d3.rs
@@ -3,20 +3,19 @@ use regex::Regex;
 
 fn d3p1(path: &str) -> i32 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    utils::line_iter(path)
-        .map(|l| {
-            let line = l.unwrap();
-            let line_sum: i32 = re
-                .captures_iter(line.as_str())
-                .map(|c| {
-                    let (_, [a_str, b_str]) = c.extract();
-                    println!("match: {:?},{:?}", a_str, b_str);
-                    let a: i32 = a_str.parse().expect("integer expected");
-                    let b: i32 = b_str.parse().expect("integer expected");
-                    a * b
-                })
-                .sum();
-            line_sum
+
+    // necessary to claim ownership of the strings
+    // can't unwrap as an iterator step because ownership would go out of scope
+    let owned_lines: Vec<String> = utils::line_iter(path).map(Result::unwrap).collect();
+    owned_lines
+        .iter()
+        .flat_map(|line| re.captures_iter(line.as_str()))
+        .map(|c| {
+            let (_, [a_str, b_str]) = c.extract();
+            println!("match: {:?},{:?}", a_str, b_str);
+            let a: i32 = a_str.parse().expect("integer expected");
+            let b: i32 = b_str.parse().expect("integer expected");
+            a * b
         })
         .sum()
 }
