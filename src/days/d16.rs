319ce2066b39f70de4577ecf0c88d1ed77de29ad
Joel Thibault <thibault@broad.dev>
2025-01-15 21:20:33 -0500

    d16p2

--- src/days/d16.rs
+++ src/days/d16.rs
@@ -1,10 +1,10 @@
 use crate::utils::matrix;
-use crate::utils::plane;
-use crate::utils::plane::Direction;
-use crate::utils::plane::MovingObject;
+use crate::utils::plane::{self, Direction, MovingObject};
+use multimap::MultiMap;
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
 const WALL: char = '#';
 const START: char = 'S';
@@ -83,25 +83,31 @@ fn get_neighbors(maze: &Vec<Vec<char>>, curr: &MovingObject) -> Vec<(MovingObjec
 fn visit_neighbor(
     to_visit: &mut PriorityQueue<MovingObject, Reverse<usize>>,
     score: &mut HashMap<MovingObject, usize>,
+    previous_nodes: &mut MultiMap<MovingObject, MovingObject>,
     current: &MovingObject,
     neighbor: &MovingObject,
     additional_score: &usize,
 ) {
     let new_score = score.get(current).unwrap() + additional_score;
     let existing_score = score.get(&neighbor).unwrap();
-    if new_score < *existing_score {
+    if new_score <= *existing_score {
         score.insert(neighbor.clone(), new_score);
+        previous_nodes.insert(neighbor.clone(), current.clone());
         if to_visit.get(neighbor).is_some() {
             to_visit.change_priority(neighbor, Reverse(new_score));
         }
     }
 }
 
-fn shortest_path(maze: &Vec<Vec<char>>) -> usize {
+fn score_maze(
+    maze: &Vec<Vec<char>>,
+) -> (
+    HashMap<MovingObject, usize>,
+    MultiMap<MovingObject, MovingObject>,
+) {
     let (mut to_visit, mut score) = init_structs(maze);
 
     let (start_row, start_col) = plane::find_unique_element(&maze, START);
-    let (end_row, end_col) = plane::find_unique_element(&maze, END);
 
     let start_node = MovingObject {
         row: start_row,
@@ -112,6 +118,8 @@ fn shortest_path(maze: &Vec<Vec<char>>) -> usize {
     to_visit.change_priority(&start_node, Reverse(0));
     score.insert(start_node, 0);
 
+    let mut previous_nodes: MultiMap<MovingObject, MovingObject> = MultiMap::new();
+
     while let Some(current) = to_visit.pop().map(|(mo, _)| mo) {
         get_neighbors(&maze, &current)
             .iter()
@@ -119,12 +127,19 @@ fn shortest_path(maze: &Vec<Vec<char>>) -> usize {
                 visit_neighbor(
                     &mut to_visit,
                     &mut score,
+                    &mut previous_nodes,
                     &current,
                     neighbor,
                     additional_score,
                 )
             });
     }
+
+    (score, previous_nodes)
+}
+
+fn min_score(maze: &Vec<Vec<char>>, score: &HashMap<MovingObject, usize>) -> usize {
+    let (end_row, end_col) = plane::find_unique_element(&maze, END);
 
     let min_score = Direction::all()
         .iter()
@@ -141,13 +156,76 @@ fn shortest_path(maze: &Vec<Vec<char>>) -> usize {
     *min_score
 }
 
+fn shortest_path(maze: &Vec<Vec<char>>) -> usize {
+    let (score, _) = score_maze(maze);
+    min_score(maze, &score)
+}
+
 pub fn d16p1(file_path: &str) -> usize {
     let maze = matrix::as_char_matrix(file_path);
     shortest_path(&maze)
 }
 
+fn shortest_path_for_node(
+    prev_nodes: &MultiMap<MovingObject, MovingObject>,
+    current: &MovingObject,
+    nodes_so_far: &HashSet<(usize, usize)>,
+    start_row: usize,
+    start_col: usize,
+) -> HashSet<(usize, usize)> {
+    if (current.row, current.col) == (start_row, start_col) {
+        return nodes_so_far.clone();
+    }
+
+    let Some(next_nodes) = prev_nodes.get_vec(&current) else {
+        return HashSet::new();
+    };
+
+    next_nodes
+        .iter()
+        .flat_map(|next_node| {
+            let mut new_nodes_so_far = nodes_so_far.clone();
+            new_nodes_so_far.insert((next_node.row, next_node.col));
+
+            shortest_path_for_node(
+                prev_nodes,
+                next_node,
+                &new_nodes_so_far,
+                start_row,
+                start_col,
+            )
+        })
+        .collect()
+}
+
+fn tiles_along_shortest_paths(maze: &Vec<Vec<char>>) -> usize {
+    let (score, prev_nodes) = score_maze(maze);
+
+    let (start_row, start_col) = plane::find_unique_element(&maze, START);
+    let (end_row, end_col) = plane::find_unique_element(&maze, END);
+
+    let shortest_paths: HashSet<(usize, usize)> = Direction::all()
+        .iter()
+        .map(|dir| MovingObject {
+            row: end_row,
+            col: end_col,
+            dir: *dir,
+            out_of_bounds: false,
+        })
+        .filter(|mo| score.get(mo).unwrap() == &min_score(maze, &score))
+        .flat_map(|node| {
+            let mut nodes_so_far: HashSet<(usize, usize)> = HashSet::new();
+            nodes_so_far.insert((node.row, node.col));
+            shortest_path_for_node(&prev_nodes, &node, &nodes_so_far, start_row, start_col)
+        })
+        .collect();
+
+    shortest_paths.len()
+}
+
 pub fn d16p2(file_path: &str) -> usize {
-    0
+    let maze = matrix::as_char_matrix(file_path);
+    tiles_along_shortest_paths(&maze)
 }
 
 pub fn d16() {
@@ -185,5 +263,17 @@ mod tests {
         ];
         // turn 1000, up 1, turn 1000, right 1
         assert_eq!(shortest_path(&maze), 2002);
+    }
+
+    #[test]
+    fn test_tiles_along_shortest_small() {
+        let mut maze = vec![
+            vec!['#', '#', '#', '#'],
+            vec!['#', '.', 'E', '#'],
+            vec!['#', 'S', '.', '#'],
+            vec!['#', '#', '#', '#'],
+        ];
+        // right 1, turn 1000, up 1
+        assert_eq!(tiles_along_shortest_paths(&maze), 3);
     }
 }
